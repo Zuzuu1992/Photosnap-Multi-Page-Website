fbdf865248fa74d69fc56088f114facac4827f2f
Ana Izoria <ana.izoria999@gmail.com>
2023-06-19 20:06:29 +0400

    docs:sort folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Features, Home, Pricing, Stories } from "./pages";
 import "./App.scss";
 
 function App() {
   return (
-    <>
-      <div>Hello World</div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/stories" element={<Stories />} />
+        <Route path="/features" element={<Features />} />
+        <Route path="/pricing" element={<Pricing />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { Home } from "./Home";
+export { Features } from "./Features";
+export { Stories } from "./Stories";
+export { Pricing } from "./Pricing";
